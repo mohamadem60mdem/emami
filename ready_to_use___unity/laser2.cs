1b8890ec2708a436ae121088cde906fdbd37f5f6
mohamadem60mdem <mohamadem60@yahoo.com>
2018-04-07 06:36:25 -0700

    س
    س

--- ready_to_use___unity/laser2.cs
+++ ready_to_use___unity/laser2.cs
@@ -6,15 +6,18 @@ using UnityEngine;
 public class laser2 : MonoBehaviour {
 
     private LineRenderer lr;
+    public GameObject prefab;
 
 
-	// Use this for initialization
-	void Start () {
+
+    // Use this for initialization
+    void Start () {
         lr = GetComponent<LineRenderer>();	
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
         RaycastHit hit;
 
         if(Physics.Raycast(transform.position,transform.forward,out hit))
@@ -22,15 +25,19 @@ public class laser2 : MonoBehaviour {
             if (hit.collider)
             {
                 lr.SetPosition(1 , new Vector3(0, 0, hit.distance));
-
+                //  Instantiate(prefab, new Vector3(0, 0, hit.distance) );Mathf.Max(1.2f, 2.4f) Mathf.Max(hit.distance)
+                //Mathf.Max(transform.position.z +hit.distance)
+              prefab.transform.position = new Vector3(hit.transform.position.x, hit.transform.position.y+5, hit.transform.position.z);
+                //rint(hit.distance);
+                print(hit.transform);
             }
 
         }
             else
             {
                 lr.SetPosition(1, new Vector3(0, 0, 5000));
-            
-            }
+            prefab.transform.position = new Vector3(0,0, 0);
+        }
 
 
        
