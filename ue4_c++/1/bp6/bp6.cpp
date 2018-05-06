9ad3c15a0054e2470597bfe21c3cd77d6ac83c61
mohamadem60mdem <mohamadem60@yahoo.com>
2018-05-06 15:22:17 +0100

    g

--- ue4_c++/1/bp6/bp6.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "bp6.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, bp6, "bp6" );
